b2caaddb8815fd73e3e4809050930cb9a3b73e7a
Vitaly Isaev <vitalyisaev2@gmail.com>
2021-11-12 11:44:04 +0300

    Clarify network address validation

--- go.mod
+++ go.mod
@@ -2,8 +2,9 @@ module github.com/lni/goutils
 
 require (
 	github.com/cockroachdb/errors v1.7.5
-	github.com/golang/protobuf v1.4.2
 	github.com/golang/snappy v0.0.1
+	github.com/pkg/errors v0.9.1
+	github.com/stretchr/testify v1.7.0
 	golang.org/x/sys v0.0.0-20200519105757-fe76b779f299
 )
 
---
+++ stringutil/regex_test.go
@@ -0,0 +1,44 @@
+package stringutil
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestIsValidAddressErr(t *testing.T) {
+	type testCase struct {
+		value    string
+		positive bool
+	}
+
+	for _, tc := range []testCase{
+		{
+			value:    "0.0.0.0:80",
+			positive: true,
+		},
+		{
+			value:    "localhost:80",
+			positive: true,
+		},
+		{
+			value:    "service-1.dc1.ru:6060",
+			positive: true,
+		},
+		{
+			// cannot use underscores in hostnames (see https://stackoverflow.com/a/2183140/2361497)
+			value:    "service_1.dc1.ru:6060",
+			positive: false,
+		},
+	} {
+		tc := tc
+		t.Run(tc.value, func(t *testing.T) {
+			err := IsValidAddressErr(tc.value)
+			if tc.positive {
+				require.NoError(t, err)
+			} else {
+				require.Error(t, err)
+			}
+		})
+	}
+}
