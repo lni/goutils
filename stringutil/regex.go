3aba1da071410537279d87e12cca36d731ef9ad7
Ben Johnson <benbjohnson@yahoo.com>
2023-12-21 07:48:43 -0700

    Fix IsValidAddress() to support leading digit (#4)

--- stringutil/regex.go
+++ stringutil/regex.go
@@ -26,7 +26,7 @@ import (
 
 var (
 	// HostnameRegex is the regex for valid hostnames.
-	HostnameRegex = regexp.MustCompile(`^(([a-zA-Z]|[a-zA-Z][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z]|[A-Za-z][A-Za-z0-9\-]*[A-Za-z0-9])$`)
+	HostnameRegex = regexp.MustCompile(`^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$`)
 	// IPV4Regex is the regex for valid IPv4 addresses.
 	IPV4Regex = regexp.MustCompile(`^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$`)
 )
